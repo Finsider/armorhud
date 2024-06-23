71772a747cc4177f9935bb2f09a37eea5749696b
Finsider <91471471+Finsider@users.noreply.github.com>
2024-06-23 14:16:55 +0700

    Add files via upload

---
+++ src/main/java/com/chailotl/better_hud/Main.java
@@ -0,0 +1,9 @@
+package com.chailotl.better_hud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Main implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient(){}
+}
---
+++ src/main/java/com/chailotl/better_hud/mixin/MixinInGameHud.java
@@ -0,0 +1,44 @@
+package com.chailotl.better_hud.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.item.ItemStack;
+import net.raphimc.immediatelyfastapi.ImmediatelyFastApi;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class MixinInGameHud
+{
+	@Final
+	@Shadow
+	private MinecraftClient client;
+
+	@Inject(
+		method = "renderHotbar",
+		at = @At(value = "TAIL")
+	)
+	private void renderArmorDurability(float tickDelta, DrawContext context, CallbackInfo ci) {
+		assert client.player != null;
+
+        int x = client.getWindow().getScaledWidth() / 2 - 7;
+		int y = client.getWindow().getScaledHeight() - 39;
+
+		ImmediatelyFastApi.getApiImpl().getBatching().beginHudBatching();
+		for (ItemStack armor : client.player.getArmorItems()) {
+			if (armor.isDamageable()) {
+				int i = armor.getItemBarStep();
+				int j = armor.getItemBarColor();
+
+				context.fill(x, y, x + 13, y + 2, 0xFF000000);
+				context.fill(x, y, x + i, y + 1, j | 0xFF000000);
+				y -= 3;
+			}
+		}
+		ImmediatelyFastApi.getApiImpl().getBatching().endHudBatching();
+	}
+}
